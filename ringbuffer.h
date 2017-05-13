263a9f1601050d85000a1bca7af54fb389f3fad0
JackZhao2017 <kobe2014zyp@126.com>
2017-05-13 22:14:34 +0800

    Create ringbuffer.h

---
+++ ringbuffer.h
@@ -0,0 +1,30 @@
+#ifndef _RINGBUFFER_H_
+#define _RINGBUFFER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define RINGBUFSIZE 256
+typedef struct{
+	int   putaddr; //put first data index;
+	int   getaddr; //get first data index;
+	int   size;	 //buffer size;
+	int   num;     //vaild data len;
+	void  *data;
+}RINGBUFFER;
+
+int ringbufferInit(RINGBUFFER *info,int size);
+int putdatatoBuffer(RINGBUFFER *info,char *buf,int len);
+int getdatafromBuffer(RINGBUFFER *info,char *buf,int len);
+int isprocessMsg(RINGBUFFER *info,int *len);
+
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
